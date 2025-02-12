751a3e2501cbe0f38434a4e0e0a6ff888d156afc
vishalicious213 <vishalicious213@gmail.com>
2025-02-11 22:33:43 -0800

    /functions/hello: change variable name from subject to name

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,7 +1,7 @@
 exports.handler = async event => {
-    const subject = event.queryStringParameters.name || 'World'
+    const name = event.queryStringParameters.name || 'World'
     return {
         statusCode: 200,
-        body: `Hello ${subject}!`,
+        body: `Hello ${name}!`,
     }
 }
