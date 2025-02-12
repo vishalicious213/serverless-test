92692f890f1d99c89d07c0273defa93b7a97cbd0
vishalicious213 <vishalicious213@gmail.com>
2025-02-11 23:08:14 -0800

    index.js: drafted sendNetlify()

--- public/index.js
+++ public/index.js
@@ -1,8 +1,22 @@
 const nameField = document.getElementById("name")
 const nameBtn = document.getElementById("nameBtn")
+const netlifyBtn = document.getElementById("sendNetlify")
+const apiBtn = document.getElementById("sendApi")
 
 nameBtn.addEventListener("click", logName)
+netlifyBtn.addEventListener("click", sendNetlify)
+apiBtn.addEventListener("click", logName)
 
 function logName() {
     console.log(nameField.value)
+}
+
+async function sendNetlify() {
+    try {
+        const response = await fetch(`/.netlify/functions/hello?name=${nameField.value}`)
+        const data = await response.json()
+        console.log(data.body)
+    } catch (error) {
+        console.log("Error fetching name response", error)
+    }
 }
